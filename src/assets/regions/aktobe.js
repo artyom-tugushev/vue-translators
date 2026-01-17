742f5f2e6880d0c43ef2763d6559b092927e45dc
artyom-tugushev <152002484+artyom-tugushev@users.noreply.github.com>
2026-01-18 01:19:45 +0300

    update translators data

--- src/assets/regions/aktobe.js
+++ src/assets/regions/aktobe.js
@@ -2,8 +2,8 @@ const aktobe = [
   {
     id: 1,
     name: 'Kirill Boreichuk',
-    phone: '+7 705 7489196',
-    messenger: '+7 705 1481865',
+    phone: '+7 (705) 748-91-96',
+    messenger: '+7 (705) 148-18-65',
     email: 'kiriusha.boreichuk@gmail.com',
     city: 'Актобе',
   },
